d71a413ddab7040114592ec00283df390c10b0e7
criempie <56305535+criempie@users.noreply.github.com>
2021-07-15 22:49:59 +0500

    add searchline

---
+++ client/src/components/SearchLine/SearchLine.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function SearchLine() {
+
+	return (
+		<input className='searchLine'
+			   type='text'
+			   placeholder='Поиск по имени' />
+	)
+}
+
+export default SearchLine
