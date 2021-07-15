1ef1a0bc1fb65ac0542ceeb7200c435a33790f38
criempie <56305535+criempie@users.noreply.github.com>
2021-07-15 22:49:46 +0500

    add font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
 import './App.css';
+import './fonts/Geometria/stylesheet.css';
+import Table, { column } from './components/Table/Table';
+import Row from './components/Table/Row';
+import SearchLine from "./components/SearchLine/SearchLine";
+import FilterNumber from "./components/Filter/FilterNumber";
+import FilterDate from "./components/Filter/FilterDate";
+
+let columns: Array<column> = [
+    {title: "№", name: "requestNumber"},
+    {title: "Время получения заявки", name: "receiveTime"},
+    {title: "Название фирмы", name: "companyName"},
+    {title: "ФИО", name: "fullname"},
+    {title: "Контактный телефон перевозчика", name: "phoneNumber"},
+    {title: "Комментарии", name: "comments"},
+    {title: "ATI код сети перевозчика", name: "atiCode"},
+    {title: "", name: "edit", width: 32},
+    {title: "", name: "trash", width: 37},
+];
+
+let rows: any = [
+    {
+        requestNumber: "123",
+        receiveTime: "21.03.21 18:02",
+        companyName: "companyName1",
+        fullname: "Иванов Иван Иванович",
+        phoneNumber: "+79233438821",
+        comments: "Круто",
+        atiCode: "https://ati.su/firms/12345/info"
+    },
+    {
+        requestNumber: "124",
+        receiveTime: "24.03.21 12:32",
+        companyName: "companyName2",
+        fullname: "Зубенко Михаил Петрович",
+        phoneNumber: "+76223433821",
+        comments: "Класс",
+        atiCode: "https://ati.su/firms/12323/info"
+    }
+]
+
 
 function App() {
   return (
     <div className="App">
+        <header className='header'></header>
+
+        <br />
+        <br />
+        <br />
+
+        <div style={{paddingLeft: "3em", paddingRight: "3em"}}>
+            <div className='inline-container'>
+                <SearchLine />
+                <FilterNumber />
+                <FilterDate />
+            </div>
+
+            <Table columns={columns}>
+                { rows.map((row: any) => <Row columns={columns} row={row} />) }
+            </Table>
+        </div>
 
     </div>
   );
